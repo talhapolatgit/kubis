7fbd4a0ccb73e4cb7bd252dee5ffe163187c9a8b
Talha Polat <talhapolat@hotmail.com>
2026-07-08 15:52:04 +0300

    updated

--- libraryProject/resources/views/auth/login.blade.php
+++ libraryProject/resources/views/auth/login.blade.php
@@ -264,7 +264,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 7v14"/><path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z"/></svg>
                 </div>
                 <div>
-                    <div class="panel-brand-name">Beyoğlu Belediyesi</div>
+                    <div class="panel-brand-name">{{ config('app.name') }}</div>
                     <div class="panel-brand-sub">Kütüphane Bilgi Sistemi</div>
                 </div>
             </div>
