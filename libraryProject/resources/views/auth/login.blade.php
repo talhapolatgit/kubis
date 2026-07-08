a5526ee01eaf6cd2b7e3912bd956e6766aa67058
Talha Polat <talhapolat@hotmail.com>
2026-07-08 15:58:08 +0300

    Updated

--- libraryProject/resources/views/auth/login.blade.php
+++ libraryProject/resources/views/auth/login.blade.php
@@ -298,7 +298,7 @@
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 7v14"/><path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z"/></svg>
                     </div>
                     <div>
-                        <div class="panel-brand-name">Beyoğlu Belediyesi</div>
+                        <div class="panel-brand-name">{{ config('app.name') }}</div>
                         <div class="panel-brand-sub">Kütüphane Bilgi Sistemi</div>
                     </div>
                 </div>
@@ -332,7 +332,7 @@
                         <svg class="input-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="20" height="16" x="2" y="4" rx="2"/><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/></svg>
                         <input type="email" class="form-input @error('email') is-error @enderror"
                                id="email" name="email" value="{{ old('email') }}"
-                               placeholder="ahmet.yilmaz@beyoglu.bel.tr" autocomplete="email" />
+                               placeholder="ahmet.yilmaz@xxx.com" autocomplete="email" />
                     </div>
                     @error('email')<div class="field-error">{{ $message }}</div>@enderror
                 </div>
